72e4852be341950935b35e54941fdc4edea2eca2
AdityaAngular01 <aditya@angularminds.in>
2025-02-12 19:19:27 +0530

    Integrated route guards and user routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-
+import { loginGuard } from './service/guards/login/login.guard';
+import { authGuard } from './service/guards/auth/auth.guard';
+import { LayoutComponent } from './user/layout/layout.component';
+import { ProfileComponent } from './user/layout/profile/profile.component';
+import { LoginComponent } from './user/auth/login/login.component';
+import { SignUpComponent } from './user/auth/sign-up/sign-up.component';
+import { MessagesComponent } from './user/layout/messages/messages.component';
+import { CreateComponent } from './user/layout/create/create.component';
+import { SearchComponent } from './user/layout/search/search.component';
+import {NotificationComponent} from './user/layout/notification/notification.component'
+import {HomeComponent} from './user/layout/home/home.component'
 export const routes: Routes = [
     {
         path: '',
@@ -10,6 +18,7 @@ export const routes: Routes = [
     },
     {
         path: 'auth',
+        canActivate: [loginGuard],
         children:[
             {
                 path:'login',
@@ -22,6 +31,42 @@ export const routes: Routes = [
             {
                 path: '',
                 redirectTo: 'login',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path:'',
+        component: LayoutComponent,
+        canActivate: [authGuard],
+        children:[
+            {
+                path: 'home',
+                component: HomeComponent
+            },
+            {
+                path: 'search',
+                component: SearchComponent
+            },
+            {
+                path: 'messages',
+                component: MessagesComponent
+            },
+            {
+                path: 'notification',
+                component: NotificationComponent
+            },
+            {
+                path: 'create',
+                component: CreateComponent
+            },
+            {
+                path: 'profile',
+                component: ProfileComponent
+            },
+            {
+                path: '',
+                redirectTo: 'home',
                 pathMatch: 'full'
             }
         ]
