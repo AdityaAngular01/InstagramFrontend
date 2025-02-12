122cad68c71ab8468e9b90c10065d6380736aab0
AdityaAngular01 <aditya@angularminds.in>
2025-02-12 19:13:56 +0530

    Defined user and auth routes in RedirectRoutes.ts

--- src/app/redirect.routes.ts
+++ src/app/redirect.routes.ts
@@ -1,10 +1,26 @@
 const auth:String = 'auth';
 const login:string = 'login';
 const register: string = 'register';
+const home:string = 'home';
+const profile: string = 'profile';
+const search:string = 'search';
+const messages: string = 'messages';
+const notification: string = 'notification';
+const create: string = 'create';
+
 
 export const RedirectRoutes = {
 	auth: {
+		base: '',
 		login: `/${auth}/${login}`,
 		register: `/${auth}/${register}`,
 	},
+	user:{
+		home: `/${home}`,
+		search: `/${search}`,
+        messages: `/${messages}`,
+        notification: `/${notification}`,
+        create: `/${create}`,
+        profile: `/${profile}`
+	}
 };
