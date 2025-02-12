1800efb01231275c8f92b97cf98bbe4fd09a3435
AdityaAngular01 <aditya@angularminds.in>
2025-02-12 09:50:40 +0530

    Main App Component Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'auth'
+    },
+    {
+        path: 'auth',
+        children:[
+            {
+                path:'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: SignUpComponent
+            },
+            {
+                path: '',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            }
+        ]
+    }
+];
