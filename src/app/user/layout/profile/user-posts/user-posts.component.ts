d53c2b2d61c76efb925cc8f249657435b0016215
AdityaAngular01 <aditya@angularminds.in>
2025-02-13 18:58:30 +0530

    Update - User Posts component to view all posts of a user

--- src/app/user/layout/profile/user-posts/user-posts.component.ts
+++ src/app/user/layout/profile/user-posts/user-posts.component.ts
@@ -1,17 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PostService } from "../../../../service/http/post/post.service";
+import { LoaderComponent } from "../../../../ReusableComponent/loader/loader.component";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { RedirectRoutes } from '../../../../redirect.routes';
 
 @Component({
 	selector: "app-user-posts",
-	imports: [],
+	imports: [LoaderComponent, CommonModule, RouterLink],
 	templateUrl: "./user-posts.component.html",
 	styleUrl: "./user-posts.component.css",
 })
-export class UserPostsComponent {
-	getArray(n: number) {
-		let array = [];
-		for (let i = 0; i < n; i++) {
-			array.push(i);
-		}
-		return array;
+export class UserPostsComponent implements OnInit {
+	protected posts: any;
+	protected RedirectRoutes = RedirectRoutes;
+	constructor(private postService: PostService) {}
+	ngOnInit(): void {
+		this.getAllPosts();
+	}
+
+	getAllPosts() {
+		this.postService.getPosts().subscribe({
+			next: (res: any) => {
+				this.posts = res.posts;
+				console.log(this.posts);
+			},
+			error: (error: any) => {
+				console.log(error.error);
+			},
+		});
+	}
+	selectedPostId: number | null = null;
+
+	toggleOverlay(postId: number) {
+		this.selectedPostId = this.selectedPostId === postId ? null : postId;
 	}
 }
